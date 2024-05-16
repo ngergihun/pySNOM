b3fde7797ffaf2abdad668609c821fb09b523b04
borondics.accounts@gmail.com <borondics@users.noreply.github.com>
2024-05-16 16:41:34 +0200

    Fixing tests for better path handling and test file content

--- pySNOM/tests/test_NeaSpectrum.py
+++ pySNOM/tests/test_NeaSpectrum.py
@@ -3,18 +3,16 @@ import os
 
 import numpy as np
 
+import pySNOM
 from pySNOM import NeaSpectrum
 
 class test_Neaspectrum(unittest.TestCase):
     def test_readfile(self):
-        test_dir = os.path.dirname(__file__)
-
         neasp = NeaSpectrum()
-        neasp.readNeaSpectrum(test_dir+'/sp.txt')
+        neasp.readNeaSpectrum(os.path.join(pySNOM.__path__[0],'datasets/sp.txt'))
 
         np.testing.assert_almost_equal(neasp.data['O1A'][0], 129.49686)
 
         
-# if __name__ == '__main__':
-#     print('running as main')
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()

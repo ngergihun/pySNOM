ebfbb7b42273f0e4e71396ba4c594cd7b03bdbad
borondics.accounts@gmail.com <borondics@users.noreply.github.com>
2024-05-16 17:18:17 +0200

    Fixed a bunch of PEP 8 style mistakes

--- pySNOM/tests/test_NeaSpectrum.py
+++ pySNOM/tests/test_NeaSpectrum.py
@@ -6,10 +6,11 @@ import numpy as np
 import pySNOM
 from pySNOM import NeaSpectrum
 
+
 class test_Neaspectrum(unittest.TestCase):
     def test_readfile(self):
         neasp = NeaSpectrum()
-        neasp.readNeaSpectrum(os.path.join(pySNOM.__path__[0],'datasets/sp.txt'))
+        neasp.readNeaSpectrum(os.path.join(pySNOM.__path__[0], 'datasets/sp.txt'))
 
         np.testing.assert_almost_equal(neasp.data['O1A'][0], 129.49686)
 

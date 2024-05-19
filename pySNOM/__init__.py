4c1ee5873428d25d7126198d2c016053584f26fd
ngergihun <46405959+ngergihun@users.noreply.github.com>
2024-05-19 12:02:10 +0200

    Update __init__.py with the version defined here

--- pySNOM/__init__.py
+++ pySNOM/__init__.py
@@ -3,7 +3,7 @@ Module pySNOM
 =============
 Scanning Near-field Optical Microscopy (SNOM) analysis tools
 """
-from .version import __version__
+__version__ = 0.0.2
 __author__ = 'Gergely Nemeth, Ferenc Borondics'
 __credits__ = 'Synchrotron SOLEIL, Wigner Research Centre for Physics'
 

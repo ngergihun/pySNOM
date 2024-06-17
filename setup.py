5f76b30eb691ea23caaffbae2f357b806133136a
borondics.accounts@gmail.com <borondics@users.noreply.github.com>
2024-06-17 15:42:28 +0200

    Setup for packaging.

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
 from setuptools import setup, find_packages
-import .pySNOM
 
-setup(
-    name='pySNOM',
-    version=pySNOM.__version__,
-    description='Scanning Near-Field Optical Microscopy (SNOM) analysis tools',
-    long_description=open('README.md', 'r').read(),
-    long_description_content_type="text/markdown",
-    url='https://github.com/ngergihun/pySNOM',
-    author='Gergely Nemeth, Ferenc Borondics',
-    author_email='ngergihun@gmail.com, borondics@gmail.com',
-    license='CC BY-NC-SA 4.0 DEED',
-    packages=find_packages(),
-    install_requires=['scipy',
-                      'numpy',
-                      'gwyfile',
-                      ],
-
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
-    ],
-)
+setup()

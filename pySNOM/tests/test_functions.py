ba377594fd4b7b0433dfdc08082b634b37c454dd
Gergely Nemeth <ngergihun@gmail.com>
2024-08-03 12:19:30 +0200

    Package test values corrected

--- pySNOM/tests/test_functions.py
+++ pySNOM/tests/test_functions.py
@@ -16,10 +16,10 @@ class test_public_functions(unittest.TestCase):
         im = Image(channeldata=channeldata)
         im.setChannel('M1A raw')
 
-        res = im.processor.normalize_simple(datatype=im.datatype)
+        res = im.processor.normalize_simple(datatype=pySNOM.images.DataTypes["Amplitude"])
 
-        np.testing.assert_almost_equal(res[4][8], -0.018852233886)
-        np.testing.assert_almost_equal(res[7][5],  2.023254394531)
+        np.testing.assert_almost_equal(res[4][8], 0.9997333773568211)
+        np.testing.assert_almost_equal(res[7][5], 1.0286144038809726)
 
     def self_reference(self):
         # TODO for testing these we need to have a better test file with actual optical data

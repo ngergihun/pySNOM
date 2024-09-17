b7a0e9d6d1d197f62b271d717a2415c382a88543
Németh Gergely <nemeth.gergely@protonmail.com>
2024-09-17 18:53:16 +0200

    Reader tests corrected for the new classes

--- pySNOM/tests/test_readers.py
+++ pySNOM/tests/test_readers.py
@@ -10,20 +10,20 @@ class TestReaders(unittest.TestCase):
     def test_gwyread(self):
         f = 'datasets/testPsHetData.gwy'
         file_reader = readers.GwyReader(fullfilepath=os.path.join(pySNOM.__path__[0], f),channelname='O3A raw')
-        data = file_reader.read()
+        gwy_image = file_reader.read()
 
-        np.testing.assert_almost_equal(data[7][7], 15.213262557983398)
-        np.testing.assert_almost_equal(data[8][8], 15.736936569213867)
-        np.testing.assert_almost_equal(data[9][9], 13.609171867370605)
+        np.testing.assert_almost_equal(gwy_image.data[7][7], 15.213262557983398)
+        np.testing.assert_almost_equal(gwy_image.data[8][8], 15.736936569213867)
+        np.testing.assert_almost_equal(gwy_image.data[9][9], 13.609171867370605)
 
     def test_readgsf(self):
         f = 'datasets/testPsHet O3A raw.gsf'
         file_reader = readers.GsfReader(os.path.join(pySNOM.__path__[0], f))
-        data = file_reader.read()
+        gwy_image = file_reader.read()
 
-        np.testing.assert_almost_equal(data[7][7], 15.213262557983398)
-        np.testing.assert_almost_equal(data[8][8], 15.736936569213867)
-        np.testing.assert_almost_equal(data[9][9], 13.609171867370605)
+        np.testing.assert_almost_equal(gwy_image.data[7][7], 15.213262557983398)
+        np.testing.assert_almost_equal(gwy_image.data[8][8], 15.736936569213867)
+        np.testing.assert_almost_equal(gwy_image.data[9][9], 13.609171867370605)
 
     def test_inforead(self):
         f = 'datasets/testinfofile.txt'

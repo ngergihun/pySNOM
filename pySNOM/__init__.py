a5de0756ed0de93060b317cfe0de0e64e706551b
Gergely Nemeth <ngergihun@gmail.com>
2024-09-30 17:43:45 +0200

    spectra and interferogram moduels, tests, testdata

--- pySNOM/__init__.py
+++ pySNOM/__init__.py
@@ -6,10 +6,8 @@ Scanning Near-field Optical Microscopy (SNOM) analysis tools
 from .defaults import defaults
 from .images import Image, Transformation
 from .readers import Reader
-from .spectra import NeaSpectrum, NeaInterferogram
-# import images
-# import spectra
-# import readers
+from .spectra import NeaSpectrum
+from .interferograms import NeaInterferogram
 
 __version__ = '0.0.2'
 __author__ = 'Gergely Németh, Ferenc Borondics'
--- pySNOM/tests/test_readers.py
+++ pySNOM/tests/test_readers.py
@@ -32,7 +32,24 @@ class TestReaders(unittest.TestCase):
 
         np.testing.assert_approx_equal(data["ScanArea"][0],5.000)
         np.testing.assert_almost_equal(data["TargetWavelength"], 6.006)
-        np.testing.assert_string_equal(data["DemodulationMode"],"PsHet")
+        np.testing.assert_string_equal(data["DemodulationMode"], "PsHet")
+
+    def test_spectrumread(self):
+        f = 'datasets/testspectrum_singlepoint.txt'
+        file_reader = readers.NeaSpectrumReader(os.path.join(pySNOM.__path__[0], f))
+        data, params = file_reader.read()
+
+        np.testing.assert_almost_equal(data['O2A'][0], 0.1600194)
+        np.testing.assert_string_equal(params["Scan"], "Fourier Scan")
+
+    def test_interferogramread(self):
+        f = 'datasets/testifg_singlepoint.txt'
+        file_reader = readers.NeaInterferogramReader(os.path.join(pySNOM.__path__[0], f))
+        data, params = file_reader.read()
+
+        np.testing.assert_almost_equal(data['O2A'][0], 9.580825)
+        np.testing.assert_almost_equal(params["Regulator"][0], 3.767854)
+        np.testing.assert_string_equal(params["Scan"], "Fourier Scan")
 
 if __name__ == '__main__':
     unittest.main()

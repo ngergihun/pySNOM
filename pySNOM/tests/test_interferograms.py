5855d719b409fb2042623600f73f90b1f826ff7e
Gergely Nemeth <ngergihun@gmail.com>
2025-03-10 18:11:21 +0100

    Reduced ifg dataset size and updated test

--- pySNOM/tests/test_interferograms.py
+++ pySNOM/tests/test_interferograms.py
@@ -22,29 +22,29 @@ class test_Neaspectrum(unittest.TestCase):
         np.testing.assert_almost_equal(self.ifg.data['O2A'][100], 9.564073)
         np.testing.assert_string_equal(self.ifg.parameters["Scan"], "Fourier Scan")
         np.testing.assert_string_equal(self.ifg.scantype, "Point")
-        np.testing.assert_equal(np.shape(self.ifg.data["O2A"])[0],46080)
+        np.testing.assert_equal(np.shape(self.ifg.data["O2A"])[0],2048)
 
     def test_singlechannel_process(self):
 
         a2, p2, wn2 = ProcessSingleChannel(order=2,simpleoutput=True).transform(self.ifg)
 
-        np.testing.assert_almost_equal(a2[500],65.082045184048)
-        np.testing.assert_almost_equal(p2[500],-2.2006946866734887)
-        np.testing.assert_almost_equal(wn2[500],1273.6715150986495)
+        np.testing.assert_almost_equal(a2[500],61.86039771056218)
+        np.testing.assert_almost_equal(p2[500],-2.226779496601322)
+        np.testing.assert_almost_equal(wn2[500],1273.6684340046547)
 
     def test_multichannel_process(self):
         s_multichannel = ProcessMultiChannels(apod=True).transform(self.ifg)
 
-        np.testing.assert_almost_equal(s_multichannel.data["O3A"][500],24.262286272717716)
-        np.testing.assert_almost_equal(s_multichannel.data["O3P"][500],0.3032580222572598)
-        np.testing.assert_almost_equal(s_multichannel.data["Wavenumber"][500],1273.6715150986495)
+        np.testing.assert_almost_equal(s_multichannel.data["O3A"][500],23.338979395903074)
+        np.testing.assert_almost_equal(s_multichannel.data["O3P"][500],0.26155207185723767)
+        np.testing.assert_almost_equal(s_multichannel.data["Wavenumber"][500],1273.6684340046547)
 
     def test_fullauto_process(self):
         s_allpoints = ProcessAllPoints().transform(self.ifg)
 
-        np.testing.assert_almost_equal(s_allpoints.data["O3A"][500],24.262286272717716)
-        np.testing.assert_almost_equal(s_allpoints.data["O3P"][500],0.3032580222572598)
-        np.testing.assert_almost_equal(s_allpoints.data["Wavenumber"][500],1273.6715150986495)
+        np.testing.assert_almost_equal(s_allpoints.data["O3A"][500],23.338979395903074)
+        np.testing.assert_almost_equal(s_allpoints.data["O3P"][500],0.26155207185723767)
+        np.testing.assert_almost_equal(s_allpoints.data["Wavenumber"][500],1273.6684340046547)
 
 if __name__ == '__main__':
     unittest.main()

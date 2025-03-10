25a49e8c6b4f45367640158265046ca60e704ee1
Gergely Nemeth <ngergihun@gmail.com>
2025-03-10 17:34:00 +0100

    Image stack aligner using cross-correlation

--- pySNOM/tests/test_transform.py
+++ pySNOM/tests/test_transform.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from pySNOM.images import LineLevel, DataTypes
+from pySNOM.images import LineLevel, DataTypes, AlignImageStack
 
 
 class TestLineLevel(unittest.TestCase):
@@ -53,6 +53,21 @@ class TestLineLevel(unittest.TestCase):
             out, [[0.0, 0.2, 0.6], [2.2222222, 2.7777778, 3.8888889]]
         )
 
+class TestAlignImageStack(unittest.TestCase):
 
-if __name__ == "__main__":
+    def test_stackalignment(self):
+        image1 = np.zeros((50,100))
+        image2 = np.zeros((50,100))
+        image1[10:40,10:40] = 1
+        image2[20:50,20:50] = 1
+
+        aligner = AlignImageStack()
+        shifts, crossrect = aligner.calculate([image1, image2])
+        np.testing.assert_equal(shifts, [np.asarray([-10., -10.])])
+        np.testing.assert_equal(crossrect, [10, 0, 40, 90])
+
+        out = aligner.transform([image1, image2],shifts,crossrect)
+        np.testing.assert_equal(np.shape(out), (2,29,90))
+
+if __name__ == '__main__':
     unittest.main()

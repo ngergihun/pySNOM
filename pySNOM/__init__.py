356bccb39847f25b7ed218b7a19ae146fca2a724
Marko Toplak <marko.toplak@gmail.com>
2025-07-10 13:34:03 +0200

    Release 0.2.1

--- pySNOM/__init__.py
+++ pySNOM/__init__.py
@@ -9,7 +9,7 @@ from .readers import Reader
 from .spectra import NeaSpectrum
 from .interferograms import NeaInterferogram
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Gergely Németh, Ferenc Borondics"
 __credits__ = "Wigner Research Centre for Physics, Synchrotron SOLEIL"
 __all__ = [
